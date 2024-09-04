7803b20c5d80dfebff4cbc5b63a59b7dc09ee8e0
LRhytz <rhytz.133@gmail.com>
2024-09-04 11:32:26 +0800

    changes in Android Manifest

---
+++ app/src/main/java/com/example/pawappproject/CitizenSubscriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pawappproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CitizenSubscriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_citizen_subscription)
+    }
+}
