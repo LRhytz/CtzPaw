efc230ca189c11c8a4c546840b4d9cc92946e9f9
LRhytz <rhytz.133@gmail.com>
2024-10-09 15:45:28 +0800

    Subscription added by Rhytz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.stripe:stripe-android:20.17.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.compose.ui:ui:1.5.0")
+    implementation ("androidx.compose.material:material:1.5.0")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.0")
+
 }
